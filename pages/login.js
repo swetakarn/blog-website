0307ef5242abfadec1d2d6deaa3f5681e4ca8c1b
swetakarn <swetakarn477@gmail.com>
2023-08-01 18:13:29 +0530

    updated

--- pages/login.js
+++ pages/login.js
@@ -1,36 +1,37 @@
-import React, { useState } from "react";
-import Link from 'next/link'
-import {auth} from "../firebase"
-import { useRouter } from "next/navigation";
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+"use client"
+import {useState} from 'react'
 
-  const handleLogin = async (e) => {
-    const router = useRouter()
-    e.preventDefault();
-         try{
+import Link from 'next/link'
+import {auth} from '../firebase'
+
+export default function login() {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+       const handleSubmit = async (e)=>{
+        e.preventDefault()
+
+        try{
           const result = await auth.signInWithEmailAndPassword(email,password)
           M.toast({html: `welcome ${result.user.displayName}`,classes:"green"})  
-          router.push("/")
         }catch(err){
           M.toast({html: err.message,classes:"red"})    
         }
-  };
-  return (
-    <div className="container center">
-    <h3>Plase Login!!</h3>
-     <form onSubmit={(e) => handleLogin(e)}>
-         <div className="input-field">
-             <input type="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
-             <input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
-         </div>
-         <button type="submit" className="btn #fb8c00 orange darken-1">Login</button>
-        <Link href="/signup"><h5>Dont Have a account</h5>signup</Link>
-     </form>
-    
-</div>
-  );
-};
-
-export default Login;
+        
+     }
+   
+    return (
+        <div className="container center">
+            <h3>Plase Login!!</h3>
+             <form onSubmit={(e) => handleSubmit(e)}>
+                 <div className="input-field">
+                     <input type="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                     <input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                 </div>
+                 <button type="submit" className="btn #fb8c00 orange darken-1">Login</button>
+                <Link href="/signup"><h5>Dont Have a account</h5>Signup</Link>
+             </form>
+            
+        </div>
+    )
+}
